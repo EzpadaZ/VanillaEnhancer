67912c8a8a37ba7d79d78121c40de7a1af1c1e5d
Jorge E <jorge.espadas@hotmail.com>
2023-06-13 15:42:29 -0600

    Fixed ListenerBootloader wrong class being called.

--- src/main/java/dev/ezpadaz/vanillaenhancer/Listeners/ListenerBootloader.java
+++ src/main/java/dev/ezpadaz/vanillaenhancer/Listeners/ListenerBootloader.java
@@ -1,11 +1,11 @@
 package dev.ezpadaz.vanillaenhancer.Listeners;
 
-import dev.ezpadaz.doublexp.DoubleXP;
+import dev.ezpadaz.vanillaenhancer.VanillaEnhancer;
 import org.bukkit.Bukkit;
 
 public class ListenerBootloader {
 
     public ListenerBootloader() {
-        Bukkit.getPluginManager().registerEvents(new DoubleXPEvent(), DoubleXP.getInstance());
+        Bukkit.getPluginManager().registerEvents(new DoubleXPEvent(), VanillaEnhancer.getInstance());
     }
 }
