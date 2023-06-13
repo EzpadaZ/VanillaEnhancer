54e00ba690dc3ac522160484d463df194dc45449
Jorge E <jorge.espadas@hotmail.com>
2023-06-13 14:57:22 -0600

    Project Base

---
+++ src/main/java/dev/ezpadaz/vanillaenhancer/VanillaEnhancer.java
@@ -0,0 +1,17 @@
+package dev.ezpadaz.vanillaenhancer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VanillaEnhancer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
