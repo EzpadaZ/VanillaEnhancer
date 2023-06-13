182c9c9ba007468345e8f15b90c2c838dc78404e
Jorge E <jorge.espadas@hotmail.com>
2023-06-13 16:13:15 -0600

    Updated config.yml and DoubleXP Event configuration variables.

--- src/main/java/dev/ezpadaz/vanillaenhancer/Listeners/DoubleXPEvent.java
+++ src/main/java/dev/ezpadaz/vanillaenhancer/Listeners/DoubleXPEvent.java
@@ -10,6 +10,7 @@ import org.bukkit.event.player.PlayerExpChangeEvent;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class DoubleXPEvent implements Listener {
     public static boolean isEnabled = false;
@@ -22,7 +23,7 @@ public class DoubleXPEvent implements Listener {
     @EventHandler
     public void playerExpChange(PlayerExpChangeEvent event) {
 
-        int multiplier = VanillaEnhancer.getInstance().config.getInt("multiplier");
+        int multiplier = Objects.requireNonNull(VanillaEnhancer.getInstance().config.getConfigurationSection("double-xp")).getInt("multiplier");
 
         if (multiplier == 0) {
             // In case default value is 0 (null)
@@ -32,21 +33,22 @@ public class DoubleXPEvent implements Listener {
         int obtainedExp = event.getAmount();
         int doubleExp = obtainedExp * multiplier;
         Player jugador = event.getPlayer();
+        boolean logEnabled = Objects.requireNonNull(VanillaEnhancer.getInstance().config.getConfigurationSection("double-xp")).getBoolean("log");
 
         if (isEnabled) {
             if (bannedPlayers.contains(jugador.getName())) {
-                if (VanillaEnhancer.getInstance().config.getBoolean("log") && optedInPlayers.contains(jugador.getName())) {
-                    MessageHelper.send(jugador, "You received: " + doubleExp +" XP (Reduced)");
+                if (logEnabled && optedInPlayers.contains(jugador.getName())) {
+                    MessageHelper.send(jugador, "You received: " + doubleExp + " XP (Reduced)");
                 }
                 event.setAmount(obtainedExp);
             } else {
-                if (VanillaEnhancer.getInstance().config.getBoolean("log") && optedInPlayers.contains(jugador.getName())) {
+                if (logEnabled && optedInPlayers.contains(jugador.getName())) {
                     MessageHelper.send(jugador, "You received: " + doubleExp + " (x" + multiplier + ") XP");
                 }
                 event.setAmount(doubleExp);
             }
         } else {
-            if (VanillaEnhancer.getInstance().config.getBoolean("log") && optedInPlayers.contains(jugador.getName()))
+            if (logEnabled && optedInPlayers.contains(jugador.getName()))
                 MessageHelper.send(event.getPlayer(), "You received: " + obtainedExp + " XP");
             event.setAmount(obtainedExp);
         }
@@ -55,12 +57,12 @@ public class DoubleXPEvent implements Listener {
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         if (isEnabled) {
-            int deathCooldown = VanillaEnhancer.getInstance().config.getInt("death-cooldown");
+            int deathCooldown = Objects.requireNonNull(VanillaEnhancer.getInstance().config.getConfigurationSection("double-xp")).getInt("death-cooldown");
             // if double XP is enabled we must cool down the player if he dies more than 3 times when the event is enabled.
             Player jugador = event.getEntity();
             Integer deathCount = doubleXpDeathCount.get(jugador.getName());
 
-            if(deathCount == null){
+            if (deathCount == null) {
                 deathCount = 1;
             }
 
