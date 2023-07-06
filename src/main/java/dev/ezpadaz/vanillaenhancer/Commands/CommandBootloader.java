76139ea4638b97f1266ca479fc39f589918b0dc7
SeniorBini <59753478+SeniorBini@users.noreply.github.com>
2023-07-06 14:41:18 -0600

    Creado el comando /salud, que trae información de salud actual, salud máxima, nivel de maná, nivel de hambre y saturación.

--- src/main/java/dev/ezpadaz/vanillaenhancer/Commands/CommandBootloader.java
+++ src/main/java/dev/ezpadaz/vanillaenhancer/Commands/CommandBootloader.java
@@ -4,6 +4,7 @@ import dev.ezpadaz.vanillaenhancer.Commands.Admin.Commander;
 import dev.ezpadaz.vanillaenhancer.Commands.Admin.TestCommand;
 import dev.ezpadaz.vanillaenhancer.Commands.DoubleXP.XPCommand;
 import dev.ezpadaz.vanillaenhancer.Commands.Misc.CancelarCommand;
+import dev.ezpadaz.vanillaenhancer.Commands.Misc.SaludCommand;
 import dev.ezpadaz.vanillaenhancer.Commands.Teleport.*;
 import dev.ezpadaz.vanillaenhancer.Utils.GeneralUtils;
 
@@ -20,6 +21,7 @@ public class CommandBootloader {
         GeneralUtils.registerCommand(new CancelarCommand());
         GeneralUtils.registerCommand(new TCCommand());
         GeneralUtils.registerCommand(new Commander());
+        GeneralUtils.registerCommand(new SaludCommand());
 
 
     }
