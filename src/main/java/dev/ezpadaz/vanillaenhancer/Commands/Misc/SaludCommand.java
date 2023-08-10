02228ce7cd7451fffcf47d164e74aea410064afe
Jorge E <jorge.espadas@hotmail.com>
2023-08-10 12:41:49 -0600

    VanillaEnhancer 1.1.3
    - Fixed saturation double format.

--- src/main/java/dev/ezpadaz/vanillaenhancer/Commands/Misc/SaludCommand.java
+++ src/main/java/dev/ezpadaz/vanillaenhancer/Commands/Misc/SaludCommand.java
@@ -20,7 +20,7 @@ public class SaludCommand extends BaseCommand {
         List<String> mensajes = new ArrayList<>();
         mensajes.add("Salud: &f" + GeneralUtils.formatDouble(jugador.getHealth()) + " &6/&f " + GeneralUtils.formatDouble(jugador.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue()));
         mensajes.add("Mana: &f" + ((DependencyHelper.hasAureliumSkills) ? AureliumAPI.getMana(jugador) : 0.0));
-        mensajes.add("Saturacion: &f" + jugador.getSaturation());
+        mensajes.add("Saturacion: &f" + GeneralUtils.formatDouble(jugador.getSaturation()));
         mensajes.add("Hambre: &f" + jugador.getFoodLevel());
         MessageHelper.sendMultipleMessage(jugador, "&bDatos de Jugador", "INFO", mensajes);
     }
